bed5d2dee15561f8bac776a9623096ee5b24072b
raghuveer9303 <raghuveer.9303@gmail.com>
2025-05-05 20:32:56 -0400

    Latest

--- vite.config.ts
+++ vite.config.ts
@@ -9,7 +9,7 @@ export default defineConfig(({ mode }) => ({
     host: "::",
     port: 8080,
   },
-  base: "/DentaVision/", // Set base for GitHub Pages
+  base: "/", // Set base for GitHub Pages
   plugins: [
     react(),
     mode === 'development' &&
